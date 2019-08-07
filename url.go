9b6c1b2098004a48fd7f2b32f5d3d4f3ef5b5251
Chao Guo <guochao@nutstore.net>
2019-08-07 20:09:03 +0800

    add RequireParametersInJSON

--- url.go
+++ url.go
@@ -1,6 +1,10 @@
 package middlewares
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
 // RequireParametersInQuery checks for parameters existence in query string
 func RequireParametersInQuery(keys ...string) func(http.Handler) http.Handler {
@@ -8,6 +12,34 @@ func RequireParametersInQuery(keys ...string) func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			for _, key := range keys {
 				if len(r.URL.Query().Get(key)) == 0 {
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+				next.ServeHTTP(w, r)
+			}
+		})
+	}
+}
+
+// RequireParametersInQuery checks for parameters existence in query string
+func RequireParametersInJSON(keys ...string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var values map[string]interface{}
+
+			buf, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			if err := json.Unmarshal(buf, &values); err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			for _, key := range keys {
+				if _, ok := values[key]; !ok {
 					w.WriteHeader(http.StatusBadRequest)
 					return
 				}
