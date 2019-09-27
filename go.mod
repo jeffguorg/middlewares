50a080f14b357b6ff35caced7658c93162bcfb6e
Chao Guo <guochao@nutstore.net>
2019-09-27 10:22:37 +0800

    update request logging format

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.12
 
 require (
 	github.com/getsentry/sentry-go v0.2.1
+	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-errors/errors v1.0.1
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/net v0.0.0-20190926025831-c00fd9afed17 // indirect
 )
--- logger.go
+++ logger.go
@@ -34,16 +34,37 @@ type LogEntry struct {
 }
 
 func (l LogEntry) Write(status, bytes int, elapsed time.Duration) {
-	l.flogger.Info("request completed in ", time.Now().Sub(l.start).Seconds(), " seconds")
+	l.flogger.Info("request completed in ", time.Now().Sub(l.start).String())
 }
 
 func (l LogEntry) Panic(v interface{}, stack []byte) {
 	l.flogger.Error("request failed for: ", string(stack))
 }
 
+func guestScheme(r *http.Request) string {
+	if r.TLS != nil {
+		return "https"
+	}
+	if v := r.Header.Get("X-FORWARD-PROTO"); v != "" {
+		return v
+	}
+	if v := r.Header.Get("X-FORWARD-SCHEME"); v != "" {
+		return v
+	}
+	return "http"
+}
+
 func (l LogrusFormatter) NewLogEntry(r *http.Request) middleware.LogEntry {
+	url := r.URL
+	url.Host = r.Host
+	if url.Scheme == "" {
+		url.Scheme = guestScheme(r)
+	}
+
 	return &LogEntry{
-		flogger: logrus.NewEntry(l.logger).WithFields(logrus.Fields{}),
-		start:   time.Now(),
+		flogger: logrus.NewEntry(l.logger).WithFields(logrus.Fields{
+			"URI": url,
+		}),
+		start: time.Now(),
 	}
 }
