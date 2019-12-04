ce3a373122690cdd5eecc5f436a0f61ee0660887
Chao Guo <guochao@nutstore.net>
2019-12-04 17:35:16 +0800

    add helpers to guess information from http headers of proxy

---
+++ xheaders.go
@@ -0,0 +1,35 @@
+package middlewares
+
+import "net/http"
+
+func guessRealIP(r *http.Request) string {
+	for _, key := range []string{"X-Real-IP", "X-Forwarded-For"} {
+		if val := r.Header.Get(key); len(val) > 0 {
+			return val
+		}
+	}
+	return ""
+}
+
+func guessScheme(r *http.Request) string {
+	for _, key := range []string{"X-Forwarded-Proto", "X-Forwarded-Scheme"} {
+		if val := r.Header.Get(key); len(val) > 0 {
+			return val
+		}
+	}
+	if len(r.URL.Scheme) > 0 {
+		return r.URL.Scheme
+	}
+	return "http"
+}
+
+func ParseProxyHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if len(r.RemoteAddr) == 0 {
+			r.RemoteAddr = guessRealIP(r)
+		}
+		r.URL.Scheme = guessScheme(r)
+
+		next.ServeHTTP(w, r)
+	})
+}
